a7cfa28432d80a0c3a7ec4d274052826ae6926dd
Gerov Boban <bracevci@yahoo.com>
2024-08-25 23:29:48 +0200

    speradmin.blade.php
    Superadmin.blade.php from now grab text from MySql

--- app/Http/Controllers/SuperadminController.php
+++ app/Http/Controllers/SuperadminController.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Txtwelcome;
 use Illuminate\Http\Request;
 
 class SuperadminController extends Controller
 {
     public function Superadmin() {
+        $txtwelcomes = Txtwelcome::query()->orderBy('created_at', 'desc')->get();
         
-           return view('superadmin');
-        
+           return view('superadmin',  ['txtwelcomes' => $txtwelcomes]);
+                  
     }
 }
